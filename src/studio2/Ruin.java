4551354f10e2f93754638fe02657aad48c21a33c
Alice Xu <Alice Xu@DESKTOP-GJO0G8J.dhcp.wustl.edu>
2024-01-31 14:19:15 -0600

    studio 2

---
+++ src/studio2/Ruin.java
@@ -0,0 +1,57 @@
+package studio2;
+
+import java.util.Scanner;
+
+public class Ruin {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("How much money are you starting with?");
+		int startAmount = in.nextInt(); 
+		System.out.println("What is the probability of winning each play? Please enter as decimal (ex: 0.5 is 50%).");
+		double winChance = in.nextDouble();
+		System.out.println("How much money do you need before you stop?");
+		int winLimit = in.nextInt();
+		System.out.println("How many days are you playing?");
+		int totalSimulations = in.nextInt();
+		
+		int winDays = 0;
+		
+		for(int day = 1; day <= totalSimulations; day++)
+		{
+			int amount = startAmount;
+			while(amount < winLimit && amount > 0)
+			{
+				double A = Math.random();
+				if (A < winChance)
+				{
+					System.out.println("WIN");
+					amount ++;
+				}
+				else
+				{
+					System.out.println("L :P");
+					amount = amount - 1;
+				}
+			}
+			
+			if (amount == 0)
+			{
+				System.out.println("Rip you're broke");
+			}
+			else if (amount == winLimit)
+			{
+				System.out.println("You won");
+				winDays++;
+			}
+		}
+		
+		System.out.println("You won " + winDays + " days");
+		double winRate = winDays/totalSimulations;
+		System.out.println("Your win rate is: " + winRate);
+		double ruinRate = (totalSimulations - winDays)/totalSimulations;
+		System.out.println("Your ruin rate is: " + ruinRate);
+
+	}
+}
